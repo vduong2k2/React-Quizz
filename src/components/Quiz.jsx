d6f051ec4032ec00032e09c615afb5a4ca28dd2b
vvduong2k2 <duongnguyen2002.bmt@gmail.com>
2024-03-03 21:39:41 +0700

    add logic quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import QUESTIONS from "../questions";
+import Complete from "../assets/quiz-complete.png";
+
 export default function Quiz() {
   const [userAnswer, setUserAnswer] = useState([]);
   const activeQuestionIndex = userAnswer.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswer((prevUserAnswer) => {
@@ -10,12 +13,22 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={Complete} alt="Trophy icon" />
+        <h2>Quiz Complete</h2>
+      </div>
+    );
+  }
+  const shuffledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswer.sort(() => Math.random() - 0.5);
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswer.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
